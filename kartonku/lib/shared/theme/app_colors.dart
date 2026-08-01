162e06be983f55a9c85c9bf469983ad4d391aaa2
fanTaux <farisarinanta11@gmail.com>
2026-08-01 21:06:22 +0700

    added Theme Constant for consistency

---
+++ kartonku/lib/shared/theme/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryGreen = Color(0xFF0F5B33);
+  static const background = Color(0xFFF7F8F9);
+  static const cardWhite = Colors.white;
+  static const textPrimary = Color(0xFF1A1A1A);
+  static const textSecondary = Color(0xFF757575);
+
+  // Status colors
+  static const statusAmanBg = Color(0xFFDFF3E3);
+  static const statusAmanText = Color(0xFF0F5B33);
+  static const statusTipisBg = Color(0xFFFBE6CC);
+  static const statusTipisText = Color(0xFF8A5A00);
+  static const statusHabisBg = Color(0xFFF6D3D3);
+  static const statusHabisText = Color(0xFFB3261E);
+}
